d62228341e3c88096858bce7939c3dd651870a07
jayesh jain <jayeshkjain6@gmail.com>
2022-09-18 17:02:36 +0530

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,63 @@
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
+/** @type import('hardhat/config').HardhatUserConfig */
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "http://eth-goerli/example"
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY || "0x00"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [GOERLI_PRIVATE_KEY],
+      chainId: 5,
+      blockConfirmations: 6,
+    },
+    localhost: { url: " http://127.0.0.1:8545/", chainId: 31337 },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: false,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    token: "MATIC",
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    user: {
+      default: 1,
+    },
+  },
+  // solidity: "0.8.17",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.8",
+      },
+      {
+        version: "0.6.6",
+      },
+    ],
+  },
+}
+
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners()
+
+  for (const account of accounts) {
+    console.log(account.address)
+  }
+})
