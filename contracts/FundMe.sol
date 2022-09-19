5aae94fb67cf37039b11779a900a99f3f8472525
jayesh jain <jayeshkjain6@gmail.com>
2022-09-19 19:20:43 +0530

    added more test

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -9,79 +9,102 @@ error FundMe__NotOwner();
 /** @title A contract for crowd funding
  *  @author Jayesh Jain
  *  @notice This contract is to demo a simple funding contract
- *  @dev This implements price feeds as our library    
- */
-contract FundMe {
-    using PriceConverter for uint256;
-
-    mapping(address => uint256) public addressToAmountFunded;
-    address[] public funders;
-
-    // Could we make this constant?  /* hint: no! We should make it immutable! */
-    address public /* immutable */ i_owner;
-    uint256 public constant MINIMUM_USD = 50 * 10 ** 18;
-
-    AggregatorV3Interface public priceFeed;
-
-     modifier onlyOwner {
-        // require(msg.sender == owner);
-        if (msg.sender != i_owner) revert FundMe__NotOwner();
-        _;
-    }
-
-    
-    constructor(address priceFeedAddress) {
-        i_owner = msg.sender;
-        priceFeed = AggregatorV3Interface(priceFeedAddress);
-    }
-    receive() external payable {
-        fund();
-    }
-       fallback() external payable {
-        fund();
-           }
-
-
-
-
-/** @notice This function fund this contract 
  *  @dev This implements price feeds as our library
  */
-    function fund() public payable {
-        require(msg.value.getConversionRate(priceFeed) >= MINIMUM_USD, "You need to spend more ETH!");
-        // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
-        addressToAmountFunded[msg.sender] += msg.value;
-        funders.push(msg.sender);
-    }
-    
-    function withdraw() public onlyOwner {
-        for (uint256 funderIndex=0; funderIndex < funders.length; funderIndex++){
-            address funder = funders[funderIndex];
-            addressToAmountFunded[funder] = 0;
-        }
-        funders = new address[](0);
-        // // transfer
-        // payable(msg.sender).transfer(address(this).balance);
-        // // send
-        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
-        // require(sendSuccess, "Send failed");
-        // call
-        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
-        require(callSuccess, "Call failed");
-    }
-    // Explainer from: https://solidity-by-example.org/fallback/
-    // Ether is sent to contract
-    //      is msg.data empty?
-    //          /   \ 
-    //         yes  no
-    //         /     \
-    //    receive()?  fallback() 
-    //     /   \ 
-    //   yes   no
-    //  /        \
-    //receive()  fallback()
+contract FundMe {
+  using PriceConverter for uint256;
 
- 
+  mapping(address => uint256) public s_addressToAmountFunded;
+  address[] public s_funders;
+
+  // Could we make this constant?  /* hint: no! We should make it immutable! */
+  address public immutable i_owner;
+  uint256 public constant MINIMUM_USD = 50 * 10**18;
+
+  AggregatorV3Interface public s_priceFeed;
+
+  modifier onlyOwner() {
+    // require(msg.sender == owner);
+    if (msg.sender != i_owner) revert FundMe__NotOwner();
+    _;
+  }
+
+  constructor(address priceFeedAddress) {
+    i_owner = msg.sender;
+    s_priceFeed = AggregatorV3Interface(priceFeedAddress);
+  }
+
+  receive() external payable {
+    fund();
+  }
+
+  fallback() external payable {
+    fund();
+  }
+
+  /** @notice This function fund this contract
+   *  @dev This implements price feeds as our library
+   */
+  function fund() public payable {
+    require(
+      msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD,
+      "You need to spend more ETH!"
+    );
+    // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
+    s_addressToAmountFunded[msg.sender] += msg.value;
+    s_funders.push(msg.sender);
+  }
+
+  function withdraw() public onlyOwner {
+    for (
+      uint256 funderIndex = 0;
+      funderIndex < s_funders.length;
+      funderIndex++
+    ) {
+      address funder = s_funders[funderIndex];
+      s_addressToAmountFunded[funder] = 0;
+    }
+    s_funders = new address[](0);
+    // // transfer
+    // payable(msg.sender).transfer(address(this).balance);
+    // // send
+    // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+    // require(sendSuccess, "Send failed");
+    // call
+    (bool callSuccess, ) = payable(msg.sender).call{
+      value: address(this).balance
+    }("");
+    require(callSuccess, "Call failed");
+  }
+
+  function cheaperWithdraw() public payable onlyOwner {
+    address[] memory memoryFunder = s_funders;
+    for (
+      uint256 funderIndex = 0;
+      funderIndex < memoryFunder.length;
+      funderIndex++
+    ) {
+      address funder = memoryFunder[funderIndex];
+      s_addressToAmountFunded[funder] = 0;
+    }
+    s_funders = new address[](0);
+    (bool callSuccess, ) = payable(msg.sender).call{
+      value: address(this).balance
+    }("");
+    require(callSuccess, "Call failed");
+  }
+
+  // Explainer from: https://solidity-by-example.org/fallback/
+  // Ether is sent to contract
+  //      is msg.data empty?
+  //          /   \
+  //         yes  no
+  //         /     \
+  //    receive()?  fallback()
+  //     /   \
+  //   yes   no
+  //  /        \
+  //receive()  fallback()
 }
 
 // Concepts we didn't cover yet (will cover in later sections)
--- test/unit/FundMe.test.js
+++ test/unit/FundMe.test.js
@@ -5,16 +5,17 @@ describe("FundMe", async function () {
   let fundMe
   let deployer
   let mockV3Aggregator
+  const sendValue = ethers.utils.parseEther("1")
   beforeEach(async function () {
-    depolyer = (await getNamedAccounts()).deployer
+    deployer = (await getNamedAccounts()).deployer
     await deployments.fixture(["all"])
-    fundMe = await ethers.getContract("FundMe", depolyer)
+    fundMe = await ethers.getContract("FundMe", deployer)
     mockV3Aggregator = await ethers.getContract("MockV3Aggregator", deployer)
   })
 
   describe("constructor", async function () {
     it("sets the aggregator addresses correctly", async function () {
-      const response = await fundMe.priceFeed()
+      const response = await fundMe.s_priceFeed()
       assert.equal(response, mockV3Aggregator.address)
     })
   })
@@ -22,8 +23,136 @@ describe("FundMe", async function () {
   describe("fund", async function () {
     it("Fails if you don't send enough ETH", async function () {
       await expect(fundMe.fund()).to.be.revertedWith(
-        "You need to spend more ETH"
+        "You need to spend more ETH!"
       )
+    })
+    it("Updated the amount funded data structure", async function () {
+      await fundMe.fund({ value: sendValue })
+      const response = await fundMe.s_addressToAmountFunded(deployer)
+      assert.equal(response.toString(), sendValue.toString())
+    })
+    it("Adds funder to array of funder", async function () {
+      await fundMe.fund({ value: sendValue })
+      const funder = await fundMe.s_funders(0)
+      assert.equal(funder, deployer)
+    })
+  })
+
+  describe("withdraw", async function () {
+    beforeEach(async function () {
+      await fundMe.fund({ value: sendValue })
+    })
+    it("withdraw ETH from a single s_funders", async function () {
+      //Arrange
+      const startingFundMeBalance = await fundMe.provider.getBalance(
+        fundMe.address
+      )
+      const startingDeployerBalance = await fundMe.provider.getBalance(deployer)
+
+      //Act
+      const transactionResponse = await fundMe.withdraw()
+      const transactionReceipt = await transactionResponse.wait(1)
+      const { gasUsed, effectiveGasPrice } = transactionReceipt
+      const gasCost = gasUsed.mul(effectiveGasPrice)
+      const endingFundMeBalance = await fundMe.provider.getBalance(
+        fundMe.address
+      )
+      const endingDeployerBalance = await fundMe.provider.getBalance(deployer)
+      //gasCost
+
+      //Assert
+      assert.equal(endingFundMeBalance, 0)
+      assert.equal(
+        startingFundMeBalance.add(startingDeployerBalance).toString(),
+        endingDeployerBalance.add(gasCost).toString()
+      )
+    })
+    it("allows us to withdraw with multiple s_funders", async function () {
+      //Arrange
+      const accounts = await ethers.getSigners()
+      for (let i = 1; i < 6; i++) {
+        const fundMeConnectedContract = await fundMe.connect(accounts[i])
+        await fundMeConnectedContract.fund({ value: sendValue })
+      }
+      const startingFundMeBalance = await fundMe.provider.getBalance(
+        fundMe.address
+      )
+      const startingDeployerBalance = await fundMe.provider.getBalance(deployer)
+
+      //Act
+      const transactionResponse = await fundMe.withdraw()
+      const transactionReceipt = await transactionResponse.wait(1)
+      const { gasUsed, effectiveGasPrice } = transactionReceipt
+      const gasCost = gasUsed.mul(effectiveGasPrice)
+      const endingFundMeBalance = await fundMe.provider.getBalance(
+        fundMe.address
+      )
+      const endingDeployerBalance = await fundMe.provider.getBalance(deployer)
+      //gasCost
+
+      //Assert
+      assert.equal(endingFundMeBalance, 0)
+      assert.equal(
+        startingFundMeBalance.add(startingDeployerBalance).toString(),
+        endingDeployerBalance.add(gasCost).toString()
+      )
+
+      //Make sure that the s_funders are reset properly
+      await expect(fundMe.s_funders(0)).to.be.reverted
+      for (let i = 1; i < 6; i++) {
+        assert.equal(
+          await fundMe.s_addressToAmountFunded(accounts[i].address),
+          0
+        )
+      }
+    })
+
+    it("Only allows the owner to withdraw", async function () {
+      const accounts = await ethers.getSigners()
+      const fundMeConnectedContract = await fundMe.connect(accounts[1])
+      await expect(fundMeConnectedContract.withdraw()).to.be.revertedWith(
+        "FundMe__NotOwner"
+      )
+    })
+
+    it("cheaper withdraw testing...", async function () {
+      //Arrange
+      const accounts = await ethers.getSigners()
+      for (let i = 1; i < 6; i++) {
+        const fundMeConnectedContract = await fundMe.connect(accounts[i])
+        await fundMeConnectedContract.fund({ value: sendValue })
+      }
+      const startingFundMeBalance = await fundMe.provider.getBalance(
+        fundMe.address
+      )
+      const startingDeployerBalance = await fundMe.provider.getBalance(deployer)
+
+      //Act
+      const transactionResponse = await fundMe.cheaperWithdraw()
+      const transactionReceipt = await transactionResponse.wait(1)
+      const { gasUsed, effectiveGasPrice } = transactionReceipt
+      const gasCost = gasUsed.mul(effectiveGasPrice)
+      const endingFundMeBalance = await fundMe.provider.getBalance(
+        fundMe.address
+      )
+      const endingDeployerBalance = await fundMe.provider.getBalance(deployer)
+      //gasCost
+
+      //Assert
+      assert.equal(endingFundMeBalance, 0)
+      assert.equal(
+        startingFundMeBalance.add(startingDeployerBalance).toString(),
+        endingDeployerBalance.add(gasCost).toString()
+      )
+
+      //Make sure that the s_funders are reset properly
+      await expect(fundMe.s_funders(0)).to.be.reverted
+      for (let i = 1; i < 6; i++) {
+        assert.equal(
+          await fundMe.s_addressToAmountFunded(accounts[i].address),
+          0
+        )
+      }
     })
   })
 })
